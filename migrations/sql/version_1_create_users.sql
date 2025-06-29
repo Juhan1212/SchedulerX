b52f2f7c10d260508ae3cf3fb44104c725daf827
JuhanKimSeoul <rlawngksss@gmail.com>
2025-06-29 20:06:55 +0900

    feat: Remove obsolete migration script for creating users table

--- migrations/sql/version_1_create_users.sql
+++
@@ -1,8 +0,0 @@
--- version1_create_users.sql
--- users 테이블 DROP & CREATE
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL
-);
